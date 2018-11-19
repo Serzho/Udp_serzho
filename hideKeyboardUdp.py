df7104d6544a66c082d0df76000ace6185e05684
Sergio Christ <37073572+Serzho@users.noreply.github.com>
2018-11-19 17:23:26 +0300

    Add files via upload

---
+++ hideKeyboardUdp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pynput import keyboard
+
+import socket
+import pickle as pl
+import time
+
+def SendMessage(msg, param = 0):
+    client.sendto(str(msg).encode('utf-8'), (IP, PORT))
+    
+def OnPress(key):
+    global listener
+    print("Press ", key)
+    #Остановка листенера
+    SendMessage(key)
+    if key == keyboard.Key.insert: #Остановка на нужную кнопку
+        listener.stop() #Остановка
+        
+
+def OnRelease(key):
+    print("Release ", key)
+
+IP = '192.168.8.177' #айпи сервера
+PORT = 8000 #порт сервера
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #создаем udp клиент
+
+listener = keyboard.Listener(on_press = OnPress, on_release = OnRelease)
+
+listener.start()
+
+listener.join()
